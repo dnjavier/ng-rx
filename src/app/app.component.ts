af3d22937ef5ed5162eb0825c84697ea5f9b6f1e
Javier Delgado <>
2023-04-10 07:43:29 -0600

    feat: :sparkles: [app] implements pagination controls for drivers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import { Subscription, BehaviorSubject, Observable, tap, switchMap } from 'rxjs';
 import { F1DataService } from './services/f1-data.service';
 import { DriverSeason } from './utils/drivers-season.interface';
 
@@ -11,8 +11,11 @@ import { DriverSeason } from './utils/drivers-season.interface';
 export class AppComponent implements OnInit, OnDestroy {
   public title = 'ng-rx';
   public activeTab = '';
-  public drivers?: DriverSeason[];
+  public pageDrivers?: DriverSeason[];
+  public allDrivers?: DriverSeason[];
+  public currentPageSubject = new BehaviorSubject<number>(1);
 
+  private itemsSubject = new BehaviorSubject<number>(10);
   private drivers$?: Subscription;
 
   constructor(private f1Data: F1DataService) { }
@@ -23,15 +26,32 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.drivers$?.unsubscribe();
+    this.itemsSubject.unsubscribe;
   }
 
-  public getDrivers(): void {
+  controlsChanged(controls: {page: number | null, itemsQty: number}): void {
+    if (controls.page && controls.itemsQty) {
+      this.currentPageSubject.next(controls.page);
+      this.itemsSubject.next(controls.itemsQty);
+    }
+  }
+
+  getDrivers(): void {
     this.drivers$ = this.f1Data.driversSeason$.subscribe((response) => {
-      this.drivers = response;
+      this.allDrivers = response;
+      this.setPageDrivers();
     });
   }
 
   toggleTab(selectedTab: string): void {
     this.activeTab = selectedTab;
+  }
+
+  private setPageDrivers(): void {
+    this.itemsSubject.asObservable().subscribe((value) => {
+      const start = value * (this.currentPageSubject.getValue() - 1);
+      const end = value * this.currentPageSubject.getValue();
+      this.pageDrivers = this.allDrivers?.slice(start, end);
+    });
   }
 }
