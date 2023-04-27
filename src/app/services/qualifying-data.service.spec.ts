d58086a3c090a08a86018931e68e6393ea81e485
Javier Delgado <>
2023-04-26 19:44:54 -0600

    feat: :white_check_mark: Adds unit tests for qualifying results data service.

--- src/app/services/qualifying-data.service.spec.ts
+++ src/app/services/qualifying-data.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QualifyingDataService } from './qualifying-data.service';
+import { of } from 'rxjs';
+import { mockSeasonQResults, mockSeasonRaces } from '../mocks/mockRaces';
+import { PaginationControls } from '../utils/pagination-controls.interface';
+import { F1Service } from './f1.service';
+import { RaceDataService } from './race-data.service';
 
-describe('QualifyingDataService', () => {
+fdescribe('QualifyingDataService', () => {
   let service: QualifyingDataService;
+  let mockF1Service: Partial<F1Service> = {
+    getQualifyingResultsInRaceAndSeason: () => of(mockSeasonQResults)
+  }
+  let mockRaceDataService: Partial<RaceDataService> = {
+    getSeasonRaces: () => of(mockSeasonRaces)
+  };
+  let controls: PaginationControls = {
+    page: 1,
+    itemsQty: 10,
+    start: 0,
+    end: 10
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ {
+        provide: F1Service,
+        useValue: mockF1Service
+      }, {
+        provide: RaceDataService,
+        useValue: mockRaceDataService
+      } ]
+    });
     service = TestBed.inject(QualifyingDataService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should call getResults and return Observable<QualifyingResults[]>', (done: DoneFn) => {
+    service.getResults(controls).subscribe(result => {
+      expect(result.length).toBeGreaterThan(0);
+      done();
+    });
   });
 });
